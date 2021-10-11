e43ebf57f4c62a541fe95fba02833d4ba973a16c
Samantha Wu <samanthayywu@gmail.com>
2021-10-11 14:39:07 +0100

    stateful

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+// 1) Create a new Flutter App (in this project) and output an AppBar and some text
+// below it
+// 2) Add a button which changes the text (to any other text of your choice)
+// 3) Split the app into three widgets: App, TextControl & Text
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Column(
+          children: [
+            Text('This is some text'),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Change text'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
